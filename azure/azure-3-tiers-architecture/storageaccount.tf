4bcfa6d0f6a84c33e6e38203b5ba2ea361fabb83
Thadikaran <41946619+kthadikaran@users.noreply.github.com>
2021-12-13 11:45:41 +0530

    Update storageaccount.tf

--- azure/azure-3-tiers-architecture/storageaccount.tf
+++ azure/azure-3-tiers-architecture/storageaccount.tf
@@ -1,3 +1,4 @@
+#Ref: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 resource "azurerm_storage_account" "teformdemostrac" {
     name                        = "teformdemo001stac"
     resource_group_name = azurerm_resource_group.teformdemorg.name 
